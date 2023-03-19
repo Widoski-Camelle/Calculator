43efff53c06fd21d7ed10c09039d1d5aeeaea769
Widoski <camelle-widoski.houndagba@epitech.eu>
2023-03-19 11:25:53 +0100

    Calculator project with Vue.Js

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Calculator
-This application is an calculator building with Vue.Js
+# site_front
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1 class="title">CALCULATOR</h1>
+    <Calculator />
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: "app",
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+.title {
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  border-bottom: 1px solid orangered;
+}
+</style>
